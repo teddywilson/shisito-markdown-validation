9b9d2e8a44b56b92c230427c5aa489a41fe98073
bingbongle <contact@defund12.org>
2020-11-02 09:30:23 -0500

    Add todo to write unit tests

--- test_shisito.py
+++ test_shisito.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+# TODO fill this in with unit tests
+
 import unittest
 import shisito
+
 
 class TestBasicAssertion(unittest.TestCase):
     def test_yep(self):
